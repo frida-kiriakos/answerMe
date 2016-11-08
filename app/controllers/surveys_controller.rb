c55257e25e466ac69f11441add370f9f77f51f1a
Frida Kiriakos <fkiriakos@cigital.com>
2016-11-08 07:47:22 -0800

    initial survey building

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,15 +2,32 @@ class SurveysController < ApplicationController
   before_action :authenticate_user!, except: :show
   
   def new
-  	@survey = current_user.surveys.new
+  	@survey = current_user.surveys.build()
   end
 
   def create
+  	@survey = current_user.surveys.build(survey_params)
+
+  	if @survey.save
+  	  flash[:success] = "Survey created successfully, you can now add your questions"
+  	  redirect_to add_question_path(@survey.id)
+  	else
+      render 'new'
+    end
   end
 
   def show
   end
 
   def update
+  end
+
+  def add_question
+  	print "survey_id " + params[:survey_id]
+  end
+
+  private
+  def survey_params
+  	params.require(:survey).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :surveys
 
+  get '/surveys/:survey_id/add_question', to: 'surveys#add_question', as: 'add_question'
+
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
